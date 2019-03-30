c0dc8d876752bbeda01af030ff8e21dce6e9312d
xswei <415516853@qq.com>
2019-03-29 17:31:28 -0700

    change appearance

--- main.js
+++ main.js
@@ -3,18 +3,16 @@ const fs = require('fs')
 const Bagpipe = require('bagpipe')
 const { app, BrowserWindow, dialog } = require('electron')
 const ipc = require('electron').ipcMain
+const menu = require('electron').Menu
 
 let win
+
 function createWindow() {
-    // 创建浏览器窗口
-    win = new BrowserWindow({ width: 1100, height: 700 })
-    win.webContents.openDevTools()
-    // 然后加载 app 的 index.html.
+    menu.setApplicationMenu(null)
+    win = new BrowserWindow({ width: 400, height: 600 })
+    // win.webContents.openDevTools()
     win.loadFile('main.html')
     win.on('closed', () => {
-        // 取消引用 window 对象，如果你的应用支持多窗口的话，
-        // 通常会把多个 window 对象存放在一个数组里面，
-        // 与此同时，你应该删除相应的元素。
         win = null
     })
 
